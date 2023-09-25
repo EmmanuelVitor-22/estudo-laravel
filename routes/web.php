c8954d6d6489c9602929361f22923fa7285c8cbf
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-09-25 11:53:57 -0300

    feat: CHECANDO OS INPUTS, (exemplo de metodos de request)

--- routes/web.php
+++ routes/web.php
@@ -15,30 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//ENTENDENDO REQUEST: ACESSANDO DADOS DO INPUT
+//ENTENDENDO REQUEST: CHECANDO OS INPUTS
 Route::get('/user', function (Request $request) {
 
-//    dd($request->input('token')); //laravel print o valor da chave passada
-
-//    dd($request->input('token', 'Laravel'));
-    /*caso o valor da chave passada não exista, você pode passar um valor padrao para ela;
-    para que não retorn null */
-
-
-// dd($request->input('product.curso')); //pode pegar dads vindo de um array
-//    dd($request->input()); // input sem parametro ele captura todos dados que exitem, incluindo query, inputs do POST
-
-
-//    dd($request->query('product')); // acessa a query diretamente
-//    dd($request->query('token', 'Laravel')); /*caso o valor da chave passada não exista, você pode passar um valor padrao para ela;
-//    para que não retorn null */
-
-//    dd($request->query()); //sem parametro ele captura todos dados que exitem
-
-//    dd($request->token); // acessa diretamente
-//    dd($request->only('token')); // traz apenas os inputs que forem definidas (podem ter mais de um, separados por virgula)
-//    dd($request->except()); // traz todos os inputs exceto os que forem definidas (podem ter mais de um, separados por virgula)
-
+//INSERIR CHECAGENS (VER VIDEO)
 });
 
 /**/
