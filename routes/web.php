d8f3700c07b897f9ed79f37cfc1621ed69482d93
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-09-25 11:52:26 -0300

    feat: ACESSANDO DADOS DO INPUT, (exemplo de metodos de request)

--- routes/web.php
+++ routes/web.php
@@ -15,60 +15,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//ENTENDENDO REQUEST: ACESSANDO DADOS DA URL
+//ENTENDENDO REQUEST: ACESSANDO DADOS DO INPUT
 Route::get('/user', function (Request $request) {
-    // dd($request);
 
-    //Get the current path info for the request.
-    // dd($request->path());
+//    dd($request->input('token')); //laravel print o valor da chave passada
 
-    //Get the URL (no query string) for the request.
-    // dd($request->url());
+//    dd($request->input('token', 'Laravel'));
+    /*caso o valor da chave passada não exista, você pode passar um valor padrao para ela;
+    para que não retorn null */
 
-    //Get the full URL for the request (include query string).
-    // dd($request->fullUrl());
 
-    /** Get the full URL for the request with the added query string parameters.
-     * traz a url com a query que ela tiver e une com o que for passado como parametro
-     *
-     * exemplo de uso: na pesquisa, o usuario pode usar filtro, ai o sistema reune esses filtros numa query e retorna isso na url ja mesclado
-     * @param array $query
-     * @return string
-     */
-    // dd($request->fullUrlWithQuery(['curso' => 'Teste'));
+// dd($request->input('product.curso')); //pode pegar dads vindo de um array
+//    dd($request->input()); // input sem parametro ele captura todos dados que exitem, incluindo query, inputs do POST
 
-    /**
-     * Determine if the current request URL and query string match a pattern.
-     * @param mixed ...$patterns
-     * @return bool
-     */
-    // dd($request->fullUrlIs('http://127.0.0.1:8000'));
 
-    /**
-     * Determine if the current request URI matches a pattern.
-     *
-     * @param mixed ...$patterns
-     * @return bool
-     */
-    // dd($request->is('users/*'));
+//    dd($request->query('product')); // acessa a query diretamente
+//    dd($request->query('token', 'Laravel')); /*caso o valor da chave passada não exista, você pode passar um valor padrao para ela;
+//    para que não retorn null */
 
-    /**
-     * Determine if the route name matches a given pattern.
-     * também verifica se o nome da rota é esse mesmo
-     * @param  mixed  ...$patterns
-     * @return bool
-     */
-     //dd($request->routeIs('users'));
+//    dd($request->query()); //sem parametro ele captura todos dados que exitem
 
-    //Get the request method.
-    // dd($request->method());
-
-    /**
-     * Checks if the request method is of specified type.
-     * @param string $method Uppercase request method (GET, POST etc)
-     * @return bool
-     */
-    // dd($request->isMethod('get'));
+//    dd($request->token); // acessa diretamente
+//    dd($request->only('token')); // traz apenas os inputs que forem definidas (podem ter mais de um, separados por virgula)
+//    dd($request->except()); // traz todos os inputs exceto os que forem definidas (podem ter mais de um, separados por virgula)
 
 });
 
+/**/
