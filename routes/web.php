2f204f3d36a17056fa9e0a1ad8fcf22fe53be029
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-09-25 11:39:33 -0300

    feat: ACESSANDO DADOS DA URL, (exemplo de metodos de request)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,15 +15,60 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//Injeção de dependencia (injetando dependencia da classe request_
-Route::get('/', function () {
-    return view('welcome');
+//ENTENDENDO REQUEST: ACESSANDO DADOS DA URL
+Route::get('/user', function (Request $request) {
+    // dd($request);
+
+    //Get the current path info for the request.
+    // dd($request->path());
+
+    //Get the URL (no query string) for the request.
+    // dd($request->url());
+
+    //Get the full URL for the request (include query string).
+    // dd($request->fullUrl());
+
+    /** Get the full URL for the request with the added query string parameters.
+     * traz a url com a query que ela tiver e une com o que for passado como parametro
+     *
+     * exemplo de uso: na pesquisa, o usuario pode usar filtro, ai o sistema reune esses filtros numa query e retorna isso na url ja mesclado
+     * @param array $query
+     * @return string
+     */
+    // dd($request->fullUrlWithQuery(['curso' => 'Teste'));
+
+    /**
+     * Determine if the current request URL and query string match a pattern.
+     * @param mixed ...$patterns
+     * @return bool
+     */
+    // dd($request->fullUrlIs('http://127.0.0.1:8000'));
+
+    /**
+     * Determine if the current request URI matches a pattern.
+     *
+     * @param mixed ...$patterns
+     * @return bool
+     */
+    // dd($request->is('users/*'));
+
+    /**
+     * Determine if the route name matches a given pattern.
+     * também verifica se o nome da rota é esse mesmo
+     * @param  mixed  ...$patterns
+     * @return bool
+     */
+     //dd($request->routeIs('users'));
+
+    //Get the request method.
+    // dd($request->method());
+
+    /**
+     * Checks if the request method is of specified type.
+     * @param string $method Uppercase request method (GET, POST etc)
+     * @return bool
+     */
+    // dd($request->isMethod('get'));
 
 });
 
-Route::resourceVerbs([
-    'create' => 'criar'
-]);
-
-Route::resource('users', \App\Http\Controllers\UserController::class);
-Route::get('checkout/{token}', \App\Http\Controllers\CheckOutController::class);
