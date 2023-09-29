167f533a844dbbcbf2a14785c98e6d32414d0522
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-09-29 11:15:44 -0300

    feat: colocando ordem na pilha de execução de stacks da blade

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -9,10 +9,16 @@
 @extends('layouts.default')
 
 @section('title', 'Lista')
-
+{{--dar um push pro fim da pilha--}}
 @push('css')
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 @endpush
+{{--dar um push pro inicio da pilha--}}
+@prepend('scripts')
+    <script>
+        let curso = 'laravel';
+    </script>
+@endprepend
 
 @section('sidebar')
     {{--traz o conteudo herdados do pai--}}
