59170ea2f97bdb001bac55f1d7f8e1a35e1aafec
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-09-29 11:11:44 -0300

    feat: diterivas stack e push

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,9 +3,16 @@
  --}}
 {{--Um modelo que você pode estender definirá suas próprias seções usando @yield, --}}
 {{--nas quais você poderá colocar seus próprios itens em seu arquivo de visualização.--}}
+
+{{--Ou seja, podem ser empilhados/colocados varios conteudos --}}
+{{--usado com css, scripts, ou varios outros conteudos que tenham a nescessidade de serem renderizados varias vezes--}}
 @extends('layouts.default')
 
 @section('title', 'Lista')
+
+@push('css')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+@endpush
 
 @section('sidebar')
     {{--traz o conteudo herdados do pai--}}
@@ -14,9 +21,8 @@
     aqui fica os menus
 @endsection
 
-
 @section('content')
-@each('user.user',$users,'user')
+    @each('user.user',$users,'user')
 @endsection
 
 
