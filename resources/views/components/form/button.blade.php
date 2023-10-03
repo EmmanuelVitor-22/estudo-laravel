f5973c183ec517a5c17025f3b5b19c1e212eaec1
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-10-03 11:52:25 -0300

    feat: metodos get e has, para manipular atributos

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -1,11 +1,18 @@
 {{--Esquema de mesclagem, permite que use tanto atributos internos como externos--}}
 <button {{ $attributes->class([ 'btn','btn-danger'=> $isRed])  }}>
-    {{--{{ $nome }}--}}
 
-    {{--cê pode usar o whereStartsWithmétodo para recuperar todos os atributos cujas chaves começam com uma determinada string--}}
-    {{$attributes->whereStartsWith('type')}}
 
-    {{--Por outro lado, o whereDoesntStartWithmétodo pode ser usado para excluir todos os atributos cujas chaves começam com uma determinada string:--}}
+    {{--Você pode recuperar o valor de um atributo específico usando o getmétodo:--}}
+    {{ $attributes->get('type') }}
 
-    {{ $attributes->whereDoesntStartWith('wire:model') }}
+    {{--    Se quiser verificar se um atributo está presente no componente, você pode usar o hasmétodo.
+     Este método aceita o nome do atributo como único argumento e retorna um booleano indicando se o atributo está presente ou não:--}}
+    @if($attributes->get('text-color'))
+        <div class="{{$attributes->get('text-color')}}">
+            {{ $nome }}
+        </div>
+    @else
+        {{ $nome }}
+    @endif
+
 </button>
