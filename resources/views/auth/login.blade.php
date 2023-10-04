03762ccbdf875eb7e742007c9ead769cd1520af5
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-10-04 11:04:09 -0300

    feat: exibindo erros

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,15 @@
             <img class="mb-4" src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72"
                  height="57">
             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
+            {{--            {{$erros->any()}} -> se tem ou não erro--}}
+            {{--            {{$erros->all()}} -> todos os erros--}}
+            @if($errors->any())
+                @foreach($errors->all() as $error)
+                   <li class="text-danger">
+                       {{$error}}
+                   </li>
+                @endforeach
+            @endif
             <div class="form-floating">
                 <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email">
                 <label for="floatingInput">Email address</label>
