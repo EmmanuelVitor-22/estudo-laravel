0507425ed63c4148e8e6539f7052cd43af4a1981
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-10-04 10:48:30 -0300

    feat: cria formulario

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,5 +22,6 @@ Route::get('/', function () {
 return view('welcome');
 });
 
+Route::get('/login', [AuthController::class,'login']);
 
 
